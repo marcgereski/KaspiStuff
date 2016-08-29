dbf939c6302d686fbe5bf9de4e75b71c2dda037a
marcgereski <marc.gereski@gmail.com>
2016-08-29 18:09:26 +0600

    Changed back-button href

--- src/main/java/kz/kaspi/stuff/configuration/WebMvcConfiguration.java
+++ src/main/java/kz/kaspi/stuff/configuration/WebMvcConfiguration.java
@@ -9,5 +9,6 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/all-questions");
     }
 }
