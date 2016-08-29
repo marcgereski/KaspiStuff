bc753e12f74f12b6566a5d58a9dbd0726473ebd4
marcgereski <marc.gereski@gmail.com>
2016-08-29 15:43:23 +0600

    Added token to db

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -9,7 +9,7 @@ drop sequence if exists hibernate_sequence;
 CREATE TABLE role(role_id BIGINT AUTO_INCREMENT PRIMARY KEY,right VARCHAR(128) NOT NULL);
 CREATE TABLE user(user_id BIGINT AUTO_INCREMENT PRIMARY KEY,username VARCHAR(128),pic LONGBLOB,email VARCHAR(128),fk_role_id BIGINT);
 CREATE TABLE category(category_id BIGINT,name VARCHAR(45) NOT NULL);
-CREATE TABLE credentials(fk_user_id BIGINT NOT NULL,pass VARCHAR(2048));
+CREATE TABLE credentials(fk_user_id BIGINT NOT NULL,pass VARCHAR(2048), token VARCHAR(2048));
 CREATE TABLE answers(answer_id BIGINT AUTO_INCREMENT PRIMARY KEY,information VARCHAR(128),fk_question_id BIGINT NOT NULL,fk_user_id BIGINT NOT NULL,date DATE,);
 CREATE TABLE questions(question_id BIGINT AUTO_INCREMENT PRIMARY KEY,description VARCHAR(512),text VARCHAR(512),fk_category_id INT NOT NULL,fk_user_id INT NOT NULL,date DATE);
 
