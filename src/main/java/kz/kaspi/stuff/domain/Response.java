cc626e735ca7455c51ee21ebd89a89f7343c9fde
marcgereski <marc.gereski@gmail.com>
2016-08-30 16:48:18 +0600

    Updated Response-class

--- src/main/java/kz/kaspi/stuff/domain/Response.java
+++ src/main/java/kz/kaspi/stuff/domain/Response.java
@@ -8,23 +8,6 @@ public class Response extends org.apache.catalina.connector.Response{
         ERROR, AUTHORIZATION_ERROR, DATA_ERROR, OK
     }
 
-    public static enum Code {
-        OK(200),
-        PageNotFound(404),
-        NotAcceptable(406),
-        PreconditionFailed(412);
-
-        private final int val;
-
-        Code(int val) {
-            this.val = val;
-        }
-
-        public int getVal() {
-            return val;
-        }
-    }
-
     private Status status;
     private String message;
     private final ModelMap map = new ModelMap();
